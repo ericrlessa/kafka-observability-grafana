9c3dec5ec4eb14a305049d18cf644dfa70edb0f0
Eric Rosetti Lessa <ericrlessa@gmail.com>
2023-02-07 16:06:04 -0300

    basico funcionando

--- product-mysql-producer/src/main/java/com/collabtime/product_mysql/Product.java
+++ product-mysql-producer/src/main/java/com/collabtime/product_mysql/Product.java
@@ -5,10 +5,10 @@ import java.math.BigDecimal;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Product {
