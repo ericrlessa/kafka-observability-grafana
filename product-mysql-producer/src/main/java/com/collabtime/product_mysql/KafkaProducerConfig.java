7df01c4c257d6ca2d61a51fc9356e07cff78f964
Eric Rosetti Lessa <ericrlessa@gmail.com>
2023-02-11 23:32:18 -0300

    ajuste na configuração kafkatempalete

--- product-mysql-producer/src/main/java/com/collabtime/product_mysql/KafkaProducerConfig.java
+++ product-mysql-producer/src/main/java/com/collabtime/product_mysql/KafkaProducerConfig.java
@@ -2,6 +2,7 @@ package com.collabtime.product_mysql;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -14,24 +15,12 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
-    @Bean
-    public ProducerFactory<String, String> producerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(
-          ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-          "kafka:9092");
-        configProps.put(
-          ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, 
-          StringSerializer.class);
-        configProps.put(
-          ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, 
-          StringSerializer.class);
-        return new DefaultKafkaProducerFactory<>(configProps);
-    }
+    @Autowired
+    private DefaultKafkaProducerFactory factory;
 
     @Bean
     public KafkaTemplate<String, String> kafkaTemplate() {
-        KafkaTemplate temp = new KafkaTemplate<>(producerFactory());
+        KafkaTemplate temp = new KafkaTemplate<>(factory);
         temp.setMicrometerEnabled(true);
         temp.setObservationEnabled(true);
         return temp;
